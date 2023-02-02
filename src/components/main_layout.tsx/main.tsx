c0fdd4bd2064673a3cca9a367358bf18b6f65262
RafaACT <rafaelcontrerast@gmail.com>
2023-02-01 20:15:47 -0500

    Initial commit

---
+++ src/components/main_layout.tsx/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Main = ({children}:any) => {
+
+    return(
+        <div>
+            <nav>
+                hello
+            </nav>
+            <div>
+                {children}
+            </div>
+            <footer>
+                hade studio
+            </footer>
+        </div>
+    )
+}
