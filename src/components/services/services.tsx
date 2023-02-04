f1e083a1cc7afd0c769cee4c88614029890bc375
RafaACT <rafaelcontrerast@gmail.com>
2023-02-03 20:51:35 -0500

    services looking good
    services looking organize, footer stick to the bottom

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -7,10 +7,24 @@ export const Services = ({data}:any) => {
         <div className="service-container">
             {data.map((service: any) => (
                 <div className="box">
-                    <Image src={service.image} alt={'doesnt work'} width={30} height={30} className={'image'}/>
-                    <h2 className="name">{service.name}</h2>
-                    <p className="description">{service.description}</p>
-                    <h3 className="price">{service.price}</h3>
+                <Grid container>
+                    <Grid item sm={4} className={'image'}>
+                        <Image src={service.image} alt={'doesnt work'} width={100} height={100} className={'image'}/>
+                    </Grid>
+                    <Grid item sm={8} className={'description'}>
+                        <Grid container>
+                            <Grid>
+                                <h2 className="name">{service.name}</h2>
+                            </Grid>
+                            <Grid>
+                                <p className="description">{service.description}</p>
+                            </Grid>
+                            <Grid>
+                                <h3 className="price">{service.price}</h3>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
                 </div>
             )
             )}  
